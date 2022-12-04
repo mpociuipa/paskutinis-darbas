4c76a2c4dca4985f6d4b87ca08c8d34a095c409b
mpociuipa <manthis.poc@gmail.com>
2022-12-04 18:47:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const burger = document.getElementById("burger")
+const sideNav = document.querySelector(".side-nav")
+
+burger.addEventListener("click", function(){
+    this.classList.toggle("rotate")
+    sideNav.classList.toggle("translate")
+})
+
+window.addEventListener("resize", function(){
+    if(window.innerWidth > 768){
+        burger.classList.remove("rotate")
+        sideNav.classList.remove("translate")
+    }
+})
